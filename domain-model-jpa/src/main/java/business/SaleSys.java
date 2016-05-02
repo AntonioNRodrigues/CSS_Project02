1fd0dafbd6d7c606fdaa0cb0501f635f69f54f24
AntonioNRodrigues <antonio.n.rodrigues@gmail.com>
2016-05-02 23:51:06 +0100

    .

--- domain-model-jpa/src/main/java/business/SaleSys.java
+++ domain-model-jpa/src/main/java/business/SaleSys.java
@@ -2,6 +2,8 @@ package business;
 
 import javax.persistence.EntityManagerFactory;
 
+import business.entities.TransationCatalog;
+
 /**
  * The initial object. The first domain object built during the startup.
  * 
@@ -15,25 +17,26 @@ public class SaleSys {
 	 * The add customer use case handler
 	 */
 	private AddCustomerHandler addCustomerHandler;
-	
+
 	/**
-	 * The process sale use case handler 
+	 * The process sale use case handler
 	 */
 	private ProcessSaleHandler processSaleHandler;
-	
+
 	/**
 	 * Performs the start up use case
 	 */
 	public SaleSys(EntityManagerFactory emf) {
 		CustomerCatalog customerCatalog = new CustomerCatalog(emf);
 		this.addCustomerHandler = new AddCustomerHandler(customerCatalog, new DiscountCatalog(emf));
-		this.processSaleHandler = new ProcessSaleHandler(new SaleCatalog(emf), customerCatalog, new ProductCatalog(emf));
+		this.processSaleHandler = new ProcessSaleHandler(new SaleCatalog(emf), customerCatalog, new ProductCatalog(emf),
+				new TransationCatalog(emf));
 	}
-	
+
 	/**
 	 * @return The add customer use case handler
 	 */
-	public AddCustomerHandler getAddCustomerHandler () {
+	public AddCustomerHandler getAddCustomerHandler() {
 		return addCustomerHandler;
 	}
 
