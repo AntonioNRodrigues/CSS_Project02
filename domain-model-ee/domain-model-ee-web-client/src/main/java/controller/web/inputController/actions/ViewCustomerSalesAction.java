2d4d2b20f7b540c409659e345ffd98f5450ac435
AntonioNRodrigues <antonio.n.rodrigues@gmail.com>
2016-05-30 00:48:30 +0100

    remove prints

--- domain-model-ee/domain-model-ee-web-client/src/main/java/controller/web/inputController/actions/ViewCustomerSalesAction.java
+++ domain-model-ee/domain-model-ee-web-client/src/main/java/controller/web/inputController/actions/ViewCustomerSalesAction.java
@@ -16,37 +16,33 @@ import facade.handlers.IViewCustomerSalesHandlerRemote;
 import presentation.web.model.ViewCustomerSalesModel;
 
 @Stateless
-public class ViewCustomerSalesAction extends Action{
+public class ViewCustomerSalesAction extends Action {
 
-	@EJB private IGetCustomersHandlerRemote customersHandler;
-	
-	@EJB private IViewCustomerSalesHandlerRemote getSalesHandler;
-	
+	@EJB
+	private IGetCustomersHandlerRemote customersHandler;
+
+	@EJB
+	private IViewCustomerSalesHandlerRemote getSalesHandler;
+
 	@Override
 	public void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+
 		try {
-			System.out.println("TESTE 1");
 			int customerVAT = Integer.parseInt(request.getParameter("vat"));
-			
-			System.out.println("TESTE 2");
+
 			Customer customer = customersHandler.getCustomerByVat(customerVAT);
-			
-			
-			System.out.println("TESTE 5");
-			ViewCustomerSalesModel model = new ViewCustomerSalesModel(customer.getVatNumber(), customer.getId(), customer.getDesignation(), getSalesHandler);
-			
-			System.out.println("TESTE 6");
+
+			ViewCustomerSalesModel model = new ViewCustomerSalesModel(customer.getVatNumber(), customer.getId(),
+					customer.getDesignation(), getSalesHandler);
+
 			request.setAttribute("model", model);
-			
+
 			request.getRequestDispatcher("/viewSales/viewSales.jsp").forward(request, response);
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 	}
 
-	
-	
 }
--- domain-model-ee/domain-model-ee-web-client/src/main/webapp/getSale/getSale.jsp
+++ domain-model-ee/domain-model-ee-web-client/src/main/webapp/getSale/getSale.jsp
@@ -8,7 +8,7 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>New Sale</title>
+<title>Sale Details</title>
 </head>
 <body>
 
